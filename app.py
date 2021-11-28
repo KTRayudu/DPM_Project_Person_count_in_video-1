16d2d197b3b9d8f1f873d2acc601f47c940f8817
KTRayudu <89350831+KTRayudu@users.noreply.github.com>
2021-11-28 17:09:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+import threading
+from bokeh.models.sources import ColumnDataSource
+from bokeh.server.server import Server
+from tornado.ioloop import IOLoop
+from bokeh.embed import server_document
+from bokeh.resources import INLINE
+from bokeh.themes.theme import Theme
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+import object_detection_video as obj_det
+import os
+import pandas as pd
+import cProfile
+from bokeh.plotting import figure
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+# Setting up video path for user uploaded video
+vidFolder = os.path.join('static')
+app.config['UPLOAD_FOLDER'] = vidFolder
+app.config['MAX _CONTENT_LENGTH'] = 100*1024*1024
+
+def bkapp(doc):
+    DataSource = ColumnDataSource(dict(Frame_Number=[],Person_Present=[],Frame_Active=[]))
+    p = figure(width=900, height=350) # A figure on which plots will be generated
+    p.circle(x="Frame_Number",y="Person_Present",source=DataSource)
+    p.line(x="Frame_Number",y="Person_Present", source=DataSource, legend_label="Person Counter", line_width=2, line_color="blue")
+    p.step(x="Frame_Number",y="Frame_Active", source=DataSource, legend_label="Frame Active", line_width=1, line_color="red")
+    p.legend.title = "Video Statistics"
+    p.xaxis.axis_label = "Frame Number"
+    p.yaxis.axis_label = "Person Count"
+
+    def update():
+        # Need to update source stream of the figure
+        # Only extract the last row of the dynamic CSV file
+        last_row = pd.read_csv('Data Files/spatial.csv',sep=',').iloc[-1]
+        Curr_frame, PersonCount, ActivityIndicator = last_row[0], last_row[1], last_row[2]
+        print(f'Current Frame:-{Curr_frame}, Person Present:-{PersonCount}.')
+        new_data = dict(Frame_Number=[Curr_frame],Person_Present=[PersonCount],Frame_Active=[ActivityIndicator])
+        DataSource.stream(new_data,rollover=200)
+        
+    doc.add_root(p)
+    doc.add_periodic_callback(update,1000)
+    #doc.theme = Theme(filename="theme.yaml")
+
+@app.route('/', methods=["GET"]) # Only get method is allowed for input page
+def input_page():
+    video_photo = os.path.join(app.config['UPLOAD_FOLDER'],'img/video_analysis.jpeg')
+    return render_template('input_page.html',img = video_photo,js_resources=INLINE.render_js(),
+    css_resources=INLINE.render_css()).encode(encoding='UTF-8')
+
+@app.route('/display/<filename>')
+def display_video(filename):
+	#print('display_video filename: ' + filename)
+	return redirect(url_for('static', filename=filename), code=301)
+        
+@app.route('/action_page', methods=["POST"])
+def action(): # currently independent of user uploaded video
+    model, classLabels = obj_det.load_pretrained_model() # Load a pre-trained model
+    obj_det.setInputParams(model=model) # Set input parameters to the model
+    uploaded_file_path = ""
+    if 'file' not in request.files:
+        flash('Video not uploaded')
+        return redirect(request.url)
+    else:
+        file = request.files['file'] # get the video file
+        if file.filename == '':
+            flash('Please upload video in .mp4 format')
+            return redirect(request.url)
+        else: # Valid file obtained
+            filename = secure_filename(file.filename)
+            uploaded_file_path = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+            file.save(uploaded_file_path) # save the video locally in static/vid
+            flash('Video has been uploaded starting Processing now')
+    
+        video_src = uploaded_file_path # read this uploaded video
+        # THIS THREAD CREATES A LIVE CSV FROM WHICH DATA HAS TO BE READ
+        thread1 = threading.Thread(target=obj_det.real_time_detection, kwargs={'model':model,'classLabels':classLabels,
+        'video_src':video_src})
+        thread1.start() 
+        script = server_document('http://localhost:5006/bkapp') # url to bokeh application
+        return render_template('output_page.html',script = script, template="Flask",filename = filename)
+
+def bk_worker():
+    server = Server({'/bkapp':bkapp}, io_loop=IOLoop(), allow_websocket_origin=["127.0.0.1:8000"])
+    server.start()
+    server.io_loop.start()
+
+threading.Thread(target=bk_worker).start() # this thread starts bokeh app on localhost:8000
+
+if __name__ == "__main__":
+
+    app.run(debug=True, threaded = True, use_reloader=False, port=8000)
---
+++ object_detection_video.py
@@ -0,0 +1,123 @@
+import time
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+from mpu.string import str2bool
+from matplotlib.animation import FuncAnimation
+
+def load_pretrained_model():
+    config_file = 'Object Detection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt' # configuration file
+    frozen_model = 'Object Detection/frozen_inference_graph.pb' # model
+    model = cv2.dnn_DetectionModel(frozen_model, config_file) # loaded model in memory
+    classLabels = [] # List of class Labels
+    file_name = 'Object Detection/Labels.txt'
+    with open(file_name,'rt') as fpt:
+        classLabels = fpt.read().rstrip('\n').split('\n')
+    return model, classLabels
+
+def print_labels(classLabels,verbose=0):
+    print(f'This Model can detect a total of :- {len(classLabels)} Labels.')
+    if verbose == 1:
+        for idx,labels in enumerate(classLabels):
+            print(f'Label {idx+1}. {classLabels[idx]}')
+
+def run_animation(model, classLabels):
+    ani = FuncAnimation(plt.gcf(), live_person_count, interval=1000)
+    plt.show()
+
+def live_person_count(frame):
+    data_file = pd.read_csv('Data Files/spatial.csv',sep=',',index_col=0) # read the csv
+    x_vals = data_file.index.values # get x values
+    y_vals_people = data_file['Person Count'].values # get person count values
+    y_vals_motion = data_file['Activity Indicator']
+    plt.cla()
+    plt.plot(x_vals, y_vals_people, label='Live Person Count')
+    plt.fill_between(x_vals, y_vals_motion, alpha=0.5, step='pre', color='r')
+    plt.step(x_vals, y_vals_motion, label='Motion Detection', alpha=0.5)
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+def setInputParams(model, width=320, height=320):
+    model.setInputSize(width,height) # input configuration file defined this as the input size
+    model.setInputScale(1.0/127.5) # 255(all gray levels)/2
+    model.setInputMean((127.5,127.5,127.5)) # mobilenet=>[-1, 1]
+    model.setInputSwapRB(True) # input in RGB format to model
+
+def Person_count(ClassesPresent, ClassLabels): # Returns person present given ClassIndex present and all class labels
+    Person_present = 0
+    if len(ClassesPresent) == 0:
+        return Person_present
+    else: # Some classes are present
+        for idx in ClassesPresent:
+            k=int(idx)
+            if ClassLabels[k-1] == 'person':
+                Person_present = Person_present + 1
+            else:
+                pass
+    return Person_present
+
+def motion_present(frame1, frame2, motion_thresh=900):
+    diff = cv2.absdiff(frame1, frame2) # Find difference between successive frames
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY) # color the diff image Gray
+    blur = cv2.GaussianBlur(gray, (5,5), 0) # apply a gaussian blur to extract contour
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY) # For Contour extraction
+    dilated = cv2.dilate(thresh, None, iterations=3) # Image Processing technique for contour extraction
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # Find all the contours present in frame 1
+
+    for contour in contours: # iterate over all the contours in a frame1
+        if cv2.contourArea(contour) < motion_thresh: # apply area threshold to detect People among all contours
+            continue
+        else:
+            return True # some motion is present
+    return False # no motion present in frame
+
+def real_time_detection(model, classLabels, video_src='videos/street_video_1.mp4',csv_location = 'Data Files/spatial.csv', start_frame=1): # this file generates a csv datafile in REAL TIME
+    """ 
+    Input to the function:-
+     1. Path of the video file uploaded by user
+     2. CSV Location of file to store statistics to
+     3. Start Frame of video
+     4. After how many frames this function should stop
+    
+    Output from the function:-
+     1. Processed frame
+     2. Script
+     3. Div
+    """
+    cap = cv2.VideoCapture(video_src) # type 0 for live webcam feed detection
+    cap.set(cv2.CAP_PROP_POS_FRAMES,start_frame-1)
+    spatial_info = pd.DataFrame({'Person Count':[0],
+                                  'Activity Indicator':[False]})
+    spatial_info.to_csv(csv_location,sep=',',index=True, index_label='Frame Number') # Begin with empty csv file
+    while True:
+        _, frame = cap.read()
+        cap_v = cap
+        ret2, frame2 = cap_v.read()
+        if ret2 == False: # Video ended
+            break
+
+        
+        ClassIndex, confidence, bbox = model.detect(frame, confThreshold=0.6)
+        font_scale = 3
+        font = cv2.FONT_HERSHEY_PLAIN
+        people_in_frame = Person_count(ClassesPresent=ClassIndex, ClassLabels=classLabels)
+        Motion = motion_present(frame1=frame, frame2=frame2)
+        spatial_info.loc[len(spatial_info.index)] = [people_in_frame, Motion] # update the dataframe
+        #with global_holder.lock():
+        spatial_info.to_csv(path_or_buf=csv_location,sep=',',index=True, index_label='Frame Number') # update the csv
+        if(len(ClassIndex) != 0):
+            for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
+                cv2.rectangle(frame, boxes, color=(255,0,0),thickness=2 )
+                cv2.putText(frame, classLabels[ClassInd-1],(boxes[0]+10, boxes[1]+40), font, fontScale=font_scale,color=(0,255,0))
+        else:
+            pass
+        time.sleep(0.033)
+        #cv2.imshow('Real Time object detection using MobileNet SSD', frame)
+        # script, div = creator.spit_html_embedding(statistics_path=csv_location, save_locally=True)
+        # with global_holder.lock():
+        #     global_holder.Output_frame = frame
+        #     global_holder.Output_div = div
+        #     global_holder.Output_script = script
+        
+    #cv2.destroyAllWindows()
+    return frame
